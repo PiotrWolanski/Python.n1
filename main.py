e21c99928ed272d4ec7df89359e762abc3fc9bc5
Piotr Wolański <101577903+PiotrWolanski@users.noreply.github.com>
2023-02-10 23:51:22 +0000

    Add math functions and operator precedence

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 #Fundamental Data Types
 # int and float
-print(type(2 + 4))
-print(type(2 - 4))
-print(type(2 * 4))
-print(type(2 / 4)) # 0.5
+# print(type(2 + 4))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4)) # 0.5
 
-print(2 ** 3)
-print(5 // 4)
-print(5 % 4)
+# print(2 ** 3)
+# print(5 // 4)
+# print(5 % 4)
+
+# math functions
+#print(round(3.9))
+#print(abs(-20))
+
+#operator precedence
+#print((20 - 3) + 2 ** 2)
+
+# ()
+# **
+# * /
+# + -

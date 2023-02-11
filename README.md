8deb8de93f30ed25dbf7e04b23b549a14da029f5
Piotr Wolański <101577903+PiotrWolanski@users.noreply.github.com>
2023-02-11 22:21:51 +0000

    I try make something like script to Ark survival evolved

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Python.n1
 Study and notes
 
+At this stage of learning I intend to comit my notes to get used to working with git on a daily basis I have not done this before when learning Java, C#.
+
 This will be my first try of commit at Python.n1 file
 
--- main.py
+++ main.py
@@ -20,3 +20,24 @@
 # **
 # * /
 # + -
+
+#First try to make something in Ark Survival Evolved
+
+import time
+import pyautogui
+
+def press_key(key):
+    pyautogui.press(key)
+
+def press_combo(combo):
+    pyautogui.hotkey(*combo)
+
+def main():
+    press_key("a")
+    time.sleep(30)
+    press_key("esc")
+    press_combo(("4", "5"))
+    press_key("f")
+
+if __name__ == "__main__":
+    main()

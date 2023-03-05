4a0ff41675fd2b3d28a57ff6c79958756a15eb51
Piotr Wolański <101577903+PiotrWolanski@users.noreply.github.com>
2023-03-05 21:03:11 +0000

    Conditional Logic

--- main.py
+++ main.py
@@ -463,3 +463,18 @@
 # print(my_set.issubset(your_set))
 # print(your_set.issuperset(my_set))
 # print(your_set.union(my_set))
+
+# Breaking The Flow
+
+# Conditional Logic
+
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced: 
+  print('you are old enought to drive, and you have a licence!')
+elif: condition:
+else:
+ print('you are not of age!')
+
+print('okoko')

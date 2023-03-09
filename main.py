18e48eef65e77db515e524415b6d2876859525dc
Piotr Wolański <101577903+PiotrWolanski@users.noreply.github.com>
2023-03-09 22:53:04 +0000

    Truthy vs Falsey

--- main.py
+++ main.py
@@ -468,13 +468,17 @@
 
 # Conditional Logic
 
-is_old = True
-is_licenced = True
+is_old = bool('hello')
+is_licenced = bool(5)
+
+print(bool('o'))
+print(bool(0))
+# Truthy and Falsy
 
 if is_old and is_licenced: 
   print('you are old enought to drive, and you have a licence!')
-elif: condition:
+# elif: condition:
 else:
  print('you are not of age!')
 
-print('okoko')
+ print('okoko')

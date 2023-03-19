28681cc5841b38997315cc1b10d063e208a98ecd
Piotr Wolański <101577903+PiotrWolanski@users.noreply.github.com>
2023-03-19 19:30:32 +0000

    3 Topic

--- main.py
+++ main.py
@@ -549,4 +549,38 @@
 #   for x in ['a', 'b', 'c']:
 #     print(item, x)
     
-# Topic: Iterables
+# Topic: Iterables - list, dictionary, tuple, set, string
+#iterated -> one by one check each item in the collection.
+
+# user = {
+#   'name': 'Golem',
+#   'age': 5006,
+#   'can_swim': False
+# }
+
+# for item in user.items():
+#   key, value = item;
+#   print(item)
+
+# for item in user.values():
+#   print(item)
+
+# for item in user.keys():
+#   print(item)
+
+# Topic: Tricky Counter - Exercise
+
+#counter
+# my_list = [1,2,3,4,5,6,7,8,9,10]
+
+# counter = 0
+# for item in my_list:
+#   counter = counter + item
+# print(counter)
+
+# Topic: range()
+
+# for _ in range (10, 0, -2):
+#   print(list(range(10)))
+
+# Topic: enumerate()

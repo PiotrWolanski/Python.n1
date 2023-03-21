8b66cf3e979770269b759ef6684ef01c93203df1
Piotr Wolański <101577903+PiotrWolanski@users.noreply.github.com>
2023-03-21 22:46:38 +0000

    3 Topic

--- main.py
+++ main.py
@@ -585,6 +585,41 @@
 
 # Topic: enumerate()
 
-for i,char in enumerate(list(range(100))):
-  if char == 50:
-    print(f'index of 50 is: {i}')
+# for i,char in enumerate(list(range(100))):
+#   if char == 50:
+#     print(f'index of 50 is: {i}')
+
+# Topic: While Loops 1&2
+
+# i = 0
+# while i < 50:
+#   print(i)
+#   i += 1
+#   break
+# else:
+#   print('done with all the work')
+# my_list = [1,2,3]
+# for item in [1,2,3]:
+#   print(item)
+
+# i = 0
+# while i < len(my_list):
+#   print(my_list[i])
+#   i += 1
+
+# while True:
+#   response = input('say something!:')
+#   if (response == 'bye'):
+#     break
+
+# Topic: break, continue, pass
+
+my_list = [1,2,3]
+for item in my_list:
+  pass
+
+i = 0
+while i < len(my_list):
+  print(my_list[i])
+  i += 1
+  pass
